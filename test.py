3144263b4cbca93484e65ae206e91c05bad80a1a
dave-ck <daveck@protonmail.com>
2019-12-24 17:07:24 +0100

    produced a test to check if array/tensor contains nan

---
+++ test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch as T
+
+
+def check_nan(tensor_or_array):
+    if type(tensor_or_array) is np.ndarray:
+        if np.isnan(tensor_or_array).any():
+            raise ValueError("NDArray contains nan:", tensor_or_array)
+    elif type(tensor_or_array) is T.Tensor:
+        if T.isnan(tensor_or_array).any():
+            raise ValueError("Tensor contains nan:", tensor_or_array)
+    else:
+        raise ValueError("Not Tensor or Array:", tensor_or_array)

156ce997ee5e8c5d682332987c56b49e31f9d6e4
dave-ck <daveck@protonmail.com>
2020-01-12 18:42:48 +0000

    set up to replace ttt_env and ttt_gym; untested

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from board import Board
 from game import Game
 from random_play import Randy
 from phils_dq import TF_Player, Agent
+from phils_utils import plotLearning
 
 
 def evaluate_game(k, n, q):
@@ -17,16 +18,16 @@ def teach_tf(k, n, q, num_games, gamma, epsilon, batch_size, alpha, eps_end, eps
     board = Board.blank_board(n, k)
     if meta:
         # assign display_board
-        scores = []
+        scores0 = []
         eps_history = []
-        score = 0
+        score0 = 0
     for i in range(num_games):
         if meta:
             pass  # print stuff to stdout every 10 episodes, every episode, whatever
             # do some other stuff like updating eps_history
         board.reset()
         done = False
-        score = 0
+        score0 = 0
         while not done:
             observation0 = board.to_linear_array()
             action0 = brain0.chooseAction(observation0)
@@ -39,3 +40,18 @@ def teach_tf(k, n, q, num_games, gamma, epsilon, batch_size, alpha, eps_end, eps
             reward0 = board.reward(0)  # todo: update - right now AI goes 1st
             observation0_ = board.to_linear_array()  # result of taking action0 on observation0, after moves by others
             brain0.storeTransition(observation0, action0, reward0, observation0_, done)
+            if meta:
+                score0 += reward0
+                scores0.append(score0)
+    if meta:
+        x = [i + 1 for i in range(num_games)]
+        filename = "tmp/" + str(n) + "n" + str(k) + "k" + str(num_games) + 'Games' + 'Gamma' + str(gamma) + \
+                   'Alpha' + str(alpha) + 'Memory' + \
+                   str(brain0.Q_eval.fc1_dims) + '-' + str(brain0.Q_eval.fc2_dims) + '.png'
+        # todo: update when player turn implemented
+        plotLearning(x, scores, eps_history, filename)
+    if write_weights:
+        modelname0 = "models/p{p}_q{q}_n{n}_k{k}_games{games}_fc1{fc1}_fc2{fc2}_alpha{alpha}_.pth" \
+            .format(p=0, q=2, n=n, k=k, games=num_games, fc1=brain0.Q_eval.fc1_dims, fc2=brain0.Q_eval.fc2_dims,
+                    alpha=brain0.Q_eval.alpha)
+        brain0.save_model(modelname0)

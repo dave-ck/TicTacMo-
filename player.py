74decc11c0b034af0432c94cf087d54e39b119d5
dave-ck <daveck@protonmail.com>
2020-01-20 15:37:10 +0000

    moved Randy to player

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import random
 
 
 class Player(ABC):
@@ -23,3 +24,29 @@ class Player(ABC):
 
     def draw(self, config):     # called on game draws
         pass
+
+class Randy(Player):
+    def __init__(self, k, n, q):
+        super().__init__(k, n, q)
+        self.name = "Randy"
+
+    def move(self, O, X, E, turn):  # refactor to take "config" as input
+        # picks a random member of E and returns it as the "expert" move
+        move = random.sample(E, 1)[0]
+        # print("Randy plays randomly: chooses {} from {}".format(move, E))
+        return move
+
+    def reward(self, config):
+        print("Win!")
+        config.print_2d()
+        print("\n\n")
+
+    def punish(self, config):
+        print("Loss :(")
+        config.print_2d()
+        print("\n\n")
+
+    def draw(self, config):
+        print("Draw :/")
+        config.print_2d()
+        print("\n\n")

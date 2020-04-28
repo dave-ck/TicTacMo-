9ee38d0a188786e303735bee953dcf1f5ae9929e
dave-ck <daveck@protonmail.com>
2020-04-28 22:58:40 +0200

    completed playout() function returning the outcome of n_g games played out randomly

--- carlos.py
+++ carlos.py
@@ -1,25 +1,39 @@
+import time
+
+from numba import jit
+import numba
 import numpy as np
 from board import Board
 
-b = Board.blank_board(3, 2, 2)
 
-print(b.positions)
-print(b.lines)
-b.move(0)
-b.move(1)
-b.move(3)
-b.move(5)
-b.cli()
-print(b.positions)
-print(b.positions==0)
-e = (b.positions==0).nonzero()[0]
-boards5 = np.array([b.positions.copy() for _ in range(5)])
-boards5[:, 8] = 1
-boards5[3, 8] = 0
-boards5[3, 6] = 1
-print(boards5)
-boards5_lines = boards5[:, b.lines]
-print(boards5_lines)
-player = 1
-boards5_wins_player = ((boards5_lines == player).sum(axis=2) == b.n).any(axis=1)
-print(boards5_wins_player)
+@jit(nopython=True)
+def playout(positions, num_games, lines, num_pos, turn, n, q):
+    # make num_games copies of board in a 2D array
+    boards = np.ones((num_games, num_pos)) * positions
+    wins = np.zeros(num_games)  # 0 if active, Q if won by player Q
+    while not wins.all() and turn < num_pos:
+        player = (turn % q) + 1
+        empties = (boards == 0).nonzero()
+        for boardNo in range(num_games):
+            if wins[boardNo] == 0:
+                move = np.random.choice(empties[1][empties[0] == boardNo])
+                boards[boardNo, move] = player  # make a random move
+                # print("Player:", player, "made move:", move)
+                # print("Resulting board:", boards[boardNo])
+                # check for wins, and updated the wins array
+                board_lines = boards[boardNo].take(lines)
+                player_wins = ((board_lines == player).sum(axis=1) == n).any()
+                if player_wins:
+                    wins[boardNo] = player
+        turn += 1
+    return wins
+
+
+b = Board.blank_board(3, 2, 3)
+
+for n_g in [10, 100, 1000, 10000, 100000]:
+    s = time.time()
+    res = playout(b.positions, n_g, b.lines, b.num_pos, b.turn, b.n, b.q)
+    e = time.time()
+    print("Computed %d games in %5f seconds" % (n_g, e-s))
+    print(np.unique(res, return_counts=True))
